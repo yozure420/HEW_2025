366dad4301376402b649cf5a1370008a0f10f079
hina0629 <hina.0629.xxx@gmail.com>
2025-11-07 14:02:25 +0900

    商品詳細ページに試聴ボタンを追加

--- front/app.py
+++ front/app.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# DBからデータを持ってきたと仮定
+from user import User
+users = [
+    User(1,'宵待ちのアリア', '橘 奏', 250),
+    User(2,'サイレント・コード', 'Chronos Ship', 1800),
+    User(3,'砂漠の残光', 'エル・ファリド', 1500),
+    User(4,'都市迷路', '零式サウンドシステム', 300),
+    User(5,'霧と埠頭のワルツ', '藍沢 雫', 2800),
+    User(6,'無限回廊の夢', 'Astral Compass', 2000),
+    User(7,'七月のシークレット・ベース', '風見 悠', 250),
+    User(8,'ノスタルジア銀河', '星野 天音', 500)
+]
+
 @app.route('/')
 def home():
-    # DBからデータを持ってきたと仮定
-    from user import User
-    users = [
-        User('宵待ちのアリア', '橘 奏', 250),
-        User('サイレント・コード', 'Chronos Ship', 1800),
-        User('砂漠の残光', 'エル・ファリド', 1500),
-        User('都市迷路', '零式サウンドシステム', 300),
-        User('霧と埠頭のワルツ', '藍沢 雫', 2800),
-        User('無限回廊の夢', 'Astral Compass', 2000),
-        User('七月のシークレット・ベース', '風見 悠', 250),
-        User('ノスタルジア銀河', '星野 天音', 500)
-    ]
     return render_template(
         'home.html',
         users=users
@@ -60,14 +61,22 @@ def user():
 @app.route('/music_list')
 def music_list():
     return render_template(
-        'music_list.html',
-        # users = users
+        'music_list.html'
     )
 
 @app.route('/product_detail')
 def product_detail():
+    product_id = request.args.get('product_id', type=int)
+
+    product = None
+    for user in users:
+        if user.id == product_id:
+            product = user
+            break
+    
     return render_template(
-        'product_detail.html'
+        'product_detail.html',
+        product=product
     )
 
 if __name__ == '__main__':
--- front/user.py
+++ front/user.py
@@ -1,9 +1,14 @@
 class User:
-    def __init__(self, music, name, price):
+    def __init__(self, id, music, name, price):
+        self.__id = id
         self.__music = music
         self.__name = name
         self.__price = price
     
+    @property
+    def id(self):
+        return self.__id
+
     @property
     def name(self):
         return self.__name
